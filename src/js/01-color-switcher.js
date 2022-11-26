f8c96695b741ce3bd4162b38acb05236ec0ead9f
VitalikApter <aptvetal2014@gmail.com>
2022-11-26 20:58:48 +0200

    task 2
    task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+
+const ref = {
+    buttonStartEl: document.querySelector('[data-start]'),
+    buttonStopEl: document.querySelector('[data-stop]'),
+};
+
+let colorChangeInterval = null;
+
+ref.buttonStartEl.addEventListener('click', evt => {
+    colorChangeInterval = onButtonStartClick(evt);
+});
+ref.buttonStopEl.addEventListener('click', onButtonStopClick);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function onButtonStartClick(evt) {
+    evt.target.setAttribute('disabled', 'disabled');
+    return setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  function onButtonStopClick (
+    evt,
+    buttonStartEl = ref.buttonStartEl,
+    interval = colorChangeInterval
+  ) {
+    buttonStartEl.removeAttribute('disabled');
+    clearInterval(colorChangeInterval);
+  }
