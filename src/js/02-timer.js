9979bd50a87cd11059073c069539633b4765c92c
VitalikApter <aptvetal2014@gmail.com>
2022-11-26 21:10:31 +0200

    task 2
    Done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,88 +2,85 @@ import Timer from './Timer';
 import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import '../sass/_common.scss';
 import '../sass/02-timer.scss';
 
 const ref = {
-    buttonStart: document.querySelector('[data-start]'),
-    timerFields: document.querySelectorAll('.timer > .field >.value'),
+  buttonStart: document.querySelector('[data-start]'),
+  timerFields: document.querySelectorAll('.timer > .field>.value'),
 };
 
 ref.buttonStart.setAttribute('disabled', 'disabled');
 
-
 const flatpickrOptions = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onclose(selectedDates) {
-        const dateNow = new Date();
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const dateNow = new Date();
 
-        if (periodValidation(selectedDates[0], dateNow)) {
-            toggleButtonActivation(ref.buttonStart);
+    if (periodValidation(selectedDates[0], dateNow)) {
+      toggleButtonActivation(ref.buttonStart);
 
-            ref.buttonStart.addEventListener('click', () => 
-            onStartButtonClick(selectedDates[0], insertTimerIntoMarkup)
-            );
-        }
-    },
-
-    locale: {
-        firstDayOfWeek: 1,
-    },
+      ref.buttonStart.addEventListener('click', () =>
+        onStartButtonClick(selectedDates[0], insertTimerIntoMarkup)
+      );
+    }
+  },
+  locale: {
+    firstDayOfWeek: 1,
+  },
 };
-
 
 const calendar = flatpickr('#datetime-picker', flatpickrOptions);
 
-
 function periodValidation(inputDate, dateNow) {
-    if (inputDate < dateNow) {
-        Notiflix.Notify.failure('Please choose a date in future', {
-            fontsize: '20px',
-            timeout: 1000,
-        });
-        return false;
-    }
-    Notiflix.Notify.success('Click "Start" button', {
-        fontsize: '20px',
-        timeout: 2000,
+  if (inputDate < dateNow) {
+    Notiflix.Notify.failure('Please choose a date in the future', {
+      fontSize: '20px',
+      timeout: 1000,
     });
-    return true;
-};
+    return false;
+  }
+  Notiflix.Notify.success('Click "Start" button', {
+    fontSize: '20px',
+    timeout: 2000,
+  });
+  return true;
+}
 
 function toggleButtonActivation(buttonEl) {
-    if (buttonEl.hasAttribute('disabled')) {
-        buttonEl.removeAttribute('disabled');
-    }else {
-        buttonEl.setAttribute('disabled', 'disabled');
-    }
-};
+  if (buttonEl.hasAttribute('disabled')) {
+    buttonEl.removeAttribute('disabled');
+  } else {
+    buttonEl.setAttribute('disabled', 'disabled');
+  }
+}
 
 function onStartButtonClick(selectedDate, insertIntoMarkupFoo) {
-    toggleButtonActivation(ref.buttonStart);
-    const timer = new Timer({ selectedDate, insertIntoMarkupFoo });
-    timer.runTimer();
+  toggleButtonActivation(ref.buttonStart);
+  const timer = new Timer({ selectedDate, insertIntoMarkupFoo });
+  timer.runTimer();
 
-    setTimeout(() => {
-        timer.killTimer();
-    }, selectedDate - Date.now());
-};
+  setTimeout(() => {
+    timer.killTimer();
+  }, selectedDate - Date.now());
+}
 
-function insertTimerIntoMarkup (timeObj) {
-    const timerRefMap = new Map();
-    ref.timerFields.forEach(el => {
-        timerRefMap.set(Object.keys(el.dataset)[0], el);
-    });
+function insertTimerIntoMarkup(timeObj) {
+  const timerRefMap = new Map();
+  ref.timerFields.forEach(el => {
+    timerRefMap.set(Object.keys(el.dataset)[0], el);
+  });
 
-    for (const key in timeObj) {
-        if (timeObj.hasOwnProperty(key)) {
-            timerRefMap.get(key).textContent = addLeadingZero(timeObj[key]);
-        }
-    };
-};
+  for (const key in timeObj) {
+    if (timeObj.hasOwnProperty(key)) {
+      timerRefMap.get(key).textContent = addLeadingZero(timeObj[key]);
+    }
+  }
+}
 
 function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-};
+  return String(value).padStart(2, '0');
+}
--- src/js/Timer.js
+++ src/js/Timer.js
@@ -2,41 +2,40 @@ export default class Timer {
     #endOfRange = null;
     #intervalId = null;
     #onTick = null;
-
+  
     constructor({ selectedDate, insertIntoMarkupFoo }) {
-        this.#endOfRange = selectedDate;
-        this.#onTick = insertIntoMarkupFoo;
+      this.#endOfRange = selectedDate;
+      this.#onTick = insertIntoMarkupFoo;
     }
-
+  
     killTimer() {
-        clearInterval(this.#intervalId);
+      clearInterval(this.#intervalId);
     }
-
+  
     runTimer() {
-        let deltaTime = this.#endOfRange - Date.now();
+      let deltaTime = this.#endOfRange - Date.now();
+      this.#onTick(this.#convertMs(deltaTime));
+      this.#intervalId = setInterval(() => {
+        deltaTime -= 1000;
         this.#onTick(this.#convertMs(deltaTime));
-        this.#intervalId = setInterval(() => {
-            deltaTime -= 1000;
-            this.#onTick(this.#convertMs(deltaTime));
-        }, 1000);
+      }, 1000);
     }
-
+  
     #convertMs(ms) {
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      }
-}
-
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+  
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+      return { days, hours, minutes, seconds };
+    }
+  }
